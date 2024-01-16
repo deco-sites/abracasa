006e35e12546f5217f237ad891c206b764e05fef
@yuri_assuncx <killerkoll2012@gmail.com>
2024-01-16 17:55:01 -0300

    fix: new improvements on PDP

--- components/product/ProductDetails.tsx
+++ components/product/ProductDetails.tsx
@@ -1,5 +1,7 @@
 import { ProductDetailsPage } from "apps/commerce/types.ts";
 
+import Image from "apps/website/components/Image.tsx";
+
 export interface Props {
   page: ProductDetailsPage | null;
 }
@@ -7,62 +9,51 @@ export interface Props {
 export default function ProductDetails({ page }: Props) {
   if (!page) return null;
 
+  const measurementImage = page?.product?.image?.find((item) =>
+    item.name === "medidas"
+  );
+
   const description = page?.product?.description ||
     page?.product?.isVariantOf?.description;
 
   return (
-    <div
-      role="tablist"
-      class="hidden lg:tabs tabs-bordered max-w-[1200px] mx-auto px-4 lg:px-0 mt-6"
-    >
-      <input
-        type="radio"
-        name="my_tabs_1"
-        role="tab"
-        class="tab min-w-[300px] checked:text-firebrick"
-        aria-label="Sobre"
-        checked
+    <div class="hidden lg:flex flex-col max-w-[1200px] mx-auto w-full gap-3">
+      <div class="grid grid-cols-4 w-full max-w-[1200px] mx-auto px-4 lg:px-0 mt-6">
+        <a
+          href="#about-product"
+          class="text-center border-b border-b-firebrick pb-2"
+        >
+          Sobre o produto
+        </a>
+
+        <a href="#" class="text-center border-b border-b-firebrick pb-2">
+          Dimensões e detalhes
+        </a>
+
+        <a href="#" class="text-center border-b border-b-firebrick pb-2">
+          Avaliações
+        </a>
+
+        <a href="#" class="text-center border-b border-b-firebrick pb-2">
+          Perguntas e respostas
+        </a>
+      </div>
+
+      <div
+        id="about-product"
+        dangerouslySetInnerHTML={{
+          __html: description?.replace(/\r?\n/g, "<br />") || "",
+        }}
       />
-      <div role="tabpanel" class="tab-content pt-6">
-        <div
-          dangerouslySetInnerHTML={{
-            __html: description?.replace(/\r?\n/g, "<br />") || "",
-          }}
+
+      {measurementImage && measurementImage.url && (
+        <Image
+          src={measurementImage.url}
+          alt={measurementImage.alternateName || "Medidas"}
+          width={420}
+          height={420}
         />
-      </div>
-
-      <input
-        type="radio"
-        name="my_tabs_1"
-        role="tab"
-        class="tab min-w-[300px] checked:text-firebrick"
-        aria-label="Dimensões e detalhes"
-      />
-      <div role="tabpanel" class="tab-content pt-6">
-        Detalhamentos de dimensões
-      </div>
-
-      <input
-        type="radio"
-        name="my_tabs_1"
-        role="tab"
-        class="tab min-w-[300px] checked:text-firebrick"
-        aria-label="Avaliações"
-      />
-      <div role="tabpanel" class="tab-content pt-6">
-        Avaliações
-      </div>
-
-      <input
-        type="radio"
-        name="my_tabs_1"
-        role="tab"
-        class="tab min-w-[300px] checked:text-firebrick"
-        aria-label="Assista o vídeo"
-      />
-      <div role="tabpanel" class="tab-content pt-6">
-        Assista o Vídeo
-      </div>
+      )}
     </div>
   );
 }
