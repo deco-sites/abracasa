0f36f4d902cedab170c4ca13d0b8adecc5ba52e6
NetinhoSZ <netinhoskennedy@gmail.com>
2024-01-17 16:04:08 -0300

    Fix: Fixed in Section SectionNewsLetter

--- components/ui/SectionNewsLetter.tsx
+++ components/ui/SectionNewsLetter.tsx
@@ -1,8 +1,8 @@
 export default function SectionNewsLetter() {
   return (
     <div class="bg-[#B9154C] text-white max-w-full flex items-center justify-center">
-      <div class="w-full h-full bg-[#B9154C] text-center my-2 py-2 px-6">
-        <h1 class="text-xl font-bold">
+      <div class="w-full h-full bg-[#B9154C] text-center my-1 py-1 px-6">
+        <h1 class="text-xl font-bold my-0 mt-1.5">
           Receba novidades, ofertas e conteúdos exclusivos!
         </h1>
         <p class="text-xs mb-4">
@@ -15,7 +15,7 @@ export default function SectionNewsLetter() {
           <input
             type="text"
             placeholder="Seu nome"
-            class="border border-gray-200 min-w-[320px] min-h-[32px] tsxt-xs px-4 focus:outline-none focus:border-white"
+            class="border border-gray-200 min-w-[320px] min-h-[32px] text-xs px-4 focus:outline-none focus:border-white"
           />
           <input
             type="email"
@@ -24,7 +24,7 @@ export default function SectionNewsLetter() {
           />
           <button
             type="submit"
-            class="border border-solid border-[#B9154C] min-w-[140px] min-h-[32px] bg-[#2C2C2C] px-4 text-xs font-semibold focus:outline-none focus:ring-2 focus:ring-[#B9154C]focus:ring-opacity-50"
+            class="border border-solid border-[#B9154C] min-w-[140px] min-h-[32px] bg-[#2C2C2C] px-4 text-sm font-bold focus:outline-none focus:ring-2 focus:ring-[#B9154C]focus:ring-opacity-50"
           >
             Enviar
           </button>
