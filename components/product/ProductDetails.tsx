cb8c2fc8fd38d98faccc382fe5f0249fdba02443
@yuri_assuncx <killerkoll2012@gmail.com>
2024-01-23 21:55:10 -0300

    feat (PDP): added collapse on ProductDetails of mobile

--- components/product/ProductDetails.tsx
+++ components/product/ProductDetails.tsx
@@ -1,13 +1,16 @@
 import { ProductDetailsPage } from "apps/commerce/types.ts";
-
+import Icon from "$store/components/ui/Icon.tsx";
 import Image from "apps/website/components/Image.tsx";
 import ReviewsSummary from "./ReviewsSummary.tsx";
+import { FnContext } from "deco/types.ts";
 
 export interface Props {
   page: ProductDetailsPage | null;
 }
 
-export default function ProductDetails({ page }: Props) {
+export default function ProductDetails(
+  { page, device }: ReturnType<typeof loader>,
+) {
   if (!page) return null;
 
   const {
@@ -19,6 +22,8 @@ export default function ProductDetails({ page }: Props) {
     image: images = [],
   } = product;
 
+  const isDesktop = device === "desktop";
+
   const additionalInfos =
     isVariantOf?.additionalProperty?.filter((item) =>
       item?.name !== "ProdutosSimilares"
@@ -29,106 +34,225 @@ export default function ProductDetails({ page }: Props) {
   const description = product?.description || product?.isVariantOf?.description;
 
   return (
-    <div class="hidden lg:flex flex-col max-w-[1200px] mx-auto w-full gap-4">
-      <div class="grid grid-cols-4 w-full max-w-[1200px] mx-auto px-4 lg:px-0 mt-6">
-        <a
-          href="#about-product"
-          class="text-center border-b border-b-firebrick pb-2"
-        >
-          Sobre o produto
-        </a>
+    <>
+      {!isDesktop && (
+        <>
+          <div class="lg:hidden border-y w-full flex items-center justify-center py-6 mt-7 px-4">
+            <span class="text-black w-full">
+              <details class="w-full">
+                <summary class="text-sm flex items-start justify-between cursor-pointer w-full collapse collapse-arrow">
+                  <span class="flex flex-1 uppercase">
+                    Sobre o produto
+                  </span>
+                  <Icon id="ChevronDown" size={24} strokeWidth={2} />
+                </summary>
 
-        <a
-          href="#dimensions-and-details"
-          class="text-center border-b border-b-firebrick pb-2"
-        >
-          Dimensões e detalhes
-        </a>
-
-        <a
-          href="#avaliations"
-          class="text-center border-b border-b-firebrick pb-2"
-        >
-          Avaliações
-        </a>
-
-        <a
-          href="#avaliations"
-          class="text-center border-b border-b-firebrick pb-2"
-        >
-          Perguntas e respostas
-        </a>
-      </div>
-
-      <div class="flex flex-col gap-12 max-w-[87%] xl:max-w-full mx-auto">
-        <div
-          id="about-product"
-          dangerouslySetInnerHTML={{
-            __html: description?.replace(/\r?\n/g, "<br />") || "",
-          }}
-        />
-
-        <div class="flex flex-col gap-12">
-          <h1 id="dimensions-and-details" class="font-bold text-xl">
-            Dimensões e Detalhes
-          </h1>
-
-          <div class="flex items-center justify-center">
-            {measurementImage && measurementImage.url && (
-              <Image
-                src={measurementImage.url}
-                alt={measurementImage.alternateName || "Medidas"}
-                width={420}
-                height={420}
-              />
-            )}
+                {description && (
+                  <div
+                    class="text-base font-normal my-8"
+                    dangerouslySetInnerHTML={{
+                      __html: description.replace(/\r?\n/g, "<br />"),
+                    }}
+                  />
+                )}
+              </details>
+            </span>
           </div>
 
-          <ul class="flex flex-col max-w-[75%] mx-auto w-full gap-1">
-            {additionalInfos?.map((item) => (
-              <li class="flex items-center justify-between border-b border-b-[#f2f2f2] last:border-none pb-1 gap-8">
-                <span class="font-bold w-full">{item.name}</span>
+          <div class="lg:hidden border-b w-full flex items-center justify-center py-6 px-4">
+            <span class="text-black w-full">
+              <details class="w-full">
+                <summary class="text-sm flex items-start justify-between cursor-pointer w-full collapse collapse-arrow">
+                  <span class="flex flex-1 uppercase">
+                    Dimensões e detalhes
+                  </span>
+                  <Icon id="ChevronDown" size={24} strokeWidth={2} />
+                </summary>
 
-                {item?.name?.includes("Materiais")
-                  ? (
-                    <div
-                      class="w-[65%] text-justify leading-relaxed tracking-wide"
-                      dangerouslySetInnerHTML={{
-                        __html: item?.value?.replace(/\r?\n/g, "<br />") || "",
-                      }}
-                    />
-                  )
-                  : item?.name?.includes("Limpeza e cuidados")
-                  ? (
-                    <a
-                      target="_blank"
-                      aria-label="abrir manual"
-                      href={item.value}
-                      class="w-[65%] text-crimson text-end"
-                    >
-                      Abrir manual
-                    </a>
-                  )
-                  : (
-                    <span
-                      class={`${
-                        item?.name !== "Garantia" && "text-end"
-                      } w-[65%]`}
-                    >
-                      {item.value}
-                    </span>
-                  )}
-              </li>
-            ))}
-          </ul>
+                <div class="flex flex-col gap-6">
+                  <div class="flex items-center justify-center">
+                    {measurementImage && measurementImage.url && (
+                      <Image
+                        src={measurementImage.url}
+                        alt={measurementImage.alternateName || "Medidas"}
+                        width={420}
+                        height={420}
+                      />
+                    )}
+                  </div>
+
+                  <ul class="flex flex-col w-full gap-1">
+                    {additionalInfos?.map((item) => (
+                      <li class="flex items-center justify-between border-b border-b-[#f2f2f2] last:border-none pb-1 gap-8">
+                        <span class="font-bold w-full">{item.name}</span>
+
+                        {item?.name?.includes("Materiais")
+                          ? (
+                            <div
+                              class="w-[65%] text-justify leading-relaxed tracking-wide"
+                              dangerouslySetInnerHTML={{
+                                __html:
+                                  item?.value?.replace(/\r?\n/g, "<br />") ||
+                                  "",
+                              }}
+                            />
+                          )
+                          : item?.name?.includes("Limpeza e cuidados")
+                          ? (
+                            <a
+                              target="_blank"
+                              aria-label="abrir manual"
+                              href={item.value}
+                              class="w-[65%] text-crimson text-end"
+                            >
+                              Abrir manual
+                            </a>
+                          )
+                          : (
+                            <span
+                              class={`${
+                                item?.name !== "Garantia" && "text-end"
+                              } w-[65%]`}
+                            >
+                              {item.value}
+                            </span>
+                          )}
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              </details>
+            </span>
+          </div>
+
+          <div class="lg:hidden border-b w-full flex items-center justify-center py-6 px-4">
+            <span class="text-black w-full">
+              <details class="w-full">
+                <summary class="text-sm flex items-start justify-between cursor-pointer w-full collapse collapse-arrow">
+                  <span class="flex flex-1 uppercase">
+                    Avaliações
+                  </span>
+                  <Icon id="ChevronDown" size={24} strokeWidth={2} />
+                </summary>
+
+                <ReviewsSummary />
+              </details>
+            </span>
+          </div>
+        </>
+      )}
+
+      {isDesktop && (
+        <div class="hidden lg:flex flex-col max-w-[1200px] mx-auto w-full gap-4">
+          <div class="grid grid-cols-4 w-full max-w-[1200px] mx-auto px-4 lg:px-0 mt-6">
+            <a
+              href="#about-product"
+              class="text-center border-b border-b-firebrick pb-2"
+            >
+              Sobre o produto
+            </a>
+
+            <a
+              href="#dimensions-and-details"
+              class="text-center border-b border-b-firebrick pb-2"
+            >
+              Dimensões e detalhes
+            </a>
+
+            <a
+              href="#avaliations"
+              class="text-center border-b border-b-firebrick pb-2"
+            >
+              Avaliações
+            </a>
+
+            <a
+              href="#avaliations"
+              class="text-center border-b border-b-firebrick pb-2"
+            >
+              Perguntas e respostas
+            </a>
+          </div>
+
+          <div class="flex flex-col gap-12 max-w-[87%] xl:max-w-full mx-auto">
+            <div
+              id="about-product"
+              dangerouslySetInnerHTML={{
+                __html: description?.replace(/\r?\n/g, "<br />") || "",
+              }}
+            />
+
+            <div class="flex flex-col gap-12">
+              <h1 id="dimensions-and-details" class="font-bold text-xl">
+                Dimensões e Detalhes
+              </h1>
+
+              <div class="flex items-center justify-center">
+                {measurementImage && measurementImage.url && (
+                  <Image
+                    src={measurementImage.url}
+                    alt={measurementImage.alternateName || "Medidas"}
+                    width={420}
+                    height={420}
+                  />
+                )}
+              </div>
+
+              <ul class="flex flex-col max-w-[75%] mx-auto w-full gap-1">
+                {additionalInfos?.map((item) => (
+                  <li class="flex items-center justify-between border-b border-b-[#f2f2f2] last:border-none pb-1 gap-8">
+                    <span class="font-bold w-full">{item.name}</span>
+
+                    {item?.name?.includes("Materiais")
+                      ? (
+                        <div
+                          class="w-[65%] text-justify leading-relaxed tracking-wide"
+                          dangerouslySetInnerHTML={{
+                            __html: item?.value?.replace(/\r?\n/g, "<br />") ||
+                              "",
+                          }}
+                        />
+                      )
+                      : item?.name?.includes("Limpeza e cuidados")
+                      ? (
+                        <a
+                          target="_blank"
+                          aria-label="abrir manual"
+                          href={item.value}
+                          class="w-[65%] text-crimson text-end"
+                        >
+                          Abrir manual
+                        </a>
+                      )
+                      : (
+                        <span
+                          class={`${
+                            item?.name !== "Garantia" && "text-end"
+                          } w-[65%]`}
+                        >
+                          {item.value}
+                        </span>
+                      )}
+                  </li>
+                ))}
+              </ul>
+            </div>
+
+            <div class="flex flex-col gap-4">
+              <h1 id="avaliations" class="font-bold text-2xl">Avaliações</h1>
+
+              <ReviewsSummary />
+            </div>
+          </div>
         </div>
-
-        <div class="flex flex-col gap-4">
-          <h1 id="avaliations" class="font-bold text-2xl">Avaliações</h1>
-
-          <ReviewsSummary />
-        </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 }
+
+export const loader = (props: Props, req: Request, ctx: FnContext) => {
+  return {
+    ...props,
+    device: ctx.device,
+  };
+};
