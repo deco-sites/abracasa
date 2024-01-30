adb9786b077ead7859c010cbdc028093c9a0ba84
@yuri_assuncx <killerkoll2012@gmail.com>
2024-01-30 19:28:39 -0300

    fix: PartnerShips fixed

--- components/ui/PartnerShips.tsx
+++ components/ui/PartnerShips.tsx
@@ -1,8 +1,8 @@
 import Icon from "$store/components/ui/Icon.tsx";
 
 export interface Props {
-  title: string;
-  description: string;
+  title?: string;
+  description?: string;
   subTitle: string;
   socialMedia: {
     label:
@@ -26,17 +26,22 @@ export default function PartnerShips(
   return (
     <section class="flex flex-col container lg:max-w-[80%] h-full pt-2 pb-12 px-4 lg:px-0">
       <div class="w-full h-full flex flex-col items-center justify-center text-center">
-        <div class="w-full h-full my-2">
-          <h1 class="text-4xl leading-[49px] text-[#555555] my-0">
-            {title || "Parcerias"}
-          </h1>
-        </div>
-        <div class="w-full h-full">
-          <p class="text-lg leading-[25px] text-[#828282] mb-6">
-            {description ||
-              "Confira o conteúdo dos nossos parceiros direto do nosso insta."}
-          </p>
-        </div>
+        {title && (
+          <div class="w-full h-full my-2">
+            <h1 class="text-4xl leading-[49px] text-[#555555] my-0">
+              {title || "Parcerias"}
+            </h1>
+          </div>
+        )}
+
+        {description && (
+          <div class="w-full h-full">
+            <p class="text-lg leading-[25px] text-[#828282] mb-6">
+              {description ||
+                "Confira o conteúdo dos nossos parceiros direto do nosso insta."}
+            </p>
+          </div>
+        )}
 
         <div class="w-[30%] h-full mt-6 mb-2 border-b border-solid border-[#828282]" />
 
@@ -46,7 +51,7 @@ export default function PartnerShips(
           </h2>
         </div>
         <div class="w-full h-full flex items-center justify-center mt-2 gap-5">
-          {socialMedia.map((item) => (
+          {socialMedia?.map((item) => (
             <a
               href={item.link}
               title={item.description}
