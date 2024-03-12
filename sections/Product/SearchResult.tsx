8e5e761a7f7a9b43a25d8bebd46feb115b97da3a
yuriassuncx <killerkoll2012@gmail.com>
2024-03-12 10:48:56 -0300

    feat (PLP): adding PLP id to Loading Fallback

--- sections/Product/SearchResult.tsx
+++ sections/Product/SearchResult.tsx
@@ -2,7 +2,11 @@ export { default, loader } from "$store/components/search/SearchResult.tsx";
 
 export function LoadingFallback() {
   return (
-    <div style={{ height: "100vh" }} class="flex justify-center items-center">
+    <div
+      id="PLP"
+      style={{ height: "100vh" }}
+      class="flex justify-center items-center"
+    >
       <span class="loading loading-spinner" />
     </div>
   );
