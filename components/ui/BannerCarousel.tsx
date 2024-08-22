8debb21b499a3fb517875291b4654e11da677c3c
@yuri_assuncx <killerkoll2012@gmail.com>
2024-08-22 17:50:35 -0300

    style: mobile BannerCarousel height changed

--- components/ui/BannerCarousel.tsx
+++ components/ui/BannerCarousel.tsx
@@ -76,7 +76,7 @@ function BannerItem(
       id={id}
       href={image?.href ?? "#"}
       aria-label={image?.alt}
-      class="relative overflow-y-hidden w-full h-[573px] md:h-[586px]"
+      class="relative overflow-hidden w-full h-[500px] md:h-[586px]"
     >
       <Picture preload={lcp}>
         <Source
