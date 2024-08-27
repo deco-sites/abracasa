d559fd465c8035c246a71d7d3a2d8dc4dfd0f7cd
@yuri_assuncx <killerkoll2012@gmail.com>
2024-08-27 14:57:58 -0300

    feat: userInfo event added to DataLayer

--- islands/LoginElement.tsx
+++ islands/LoginElement.tsx
@@ -1,8 +1,38 @@
 import { useUser } from "apps/vtex/hooks/useUser.ts";
 import Icon from "deco-sites/abracasa/components/ui/Icon.tsx";
 
+import { sendEvent } from "$store/sdk/analytics.tsx";
+import { IS_BROWSER } from "$fresh/runtime.ts";
+import { useEffect } from "preact/compat";
+
 export default function LoginElement() {
   const { user } = useUser();
+
+  useEffect(() => {
+    if (!user.value) return;
+
+    const { email, name } = user.value;
+
+    if (IS_BROWSER && globalThis.window.dataLayer) {
+      const dataLayer = globalThis.window.dataLayer as Array<
+        { event?: string }
+      >;
+
+      const hasUserInfoEvent = dataLayer.some(
+        (item) => item.event === "user_info",
+      );
+
+      if (!hasUserInfoEvent) {
+        sendEvent({
+          name: "user_info",
+          params: {
+            email,
+            name,
+          },
+        });
+      }
+    }
+  }, [user.value]);
 
   return (
     <a
@@ -17,7 +47,7 @@ export default function LoginElement() {
         alt="User icon"
       />
       <span class="truncate tracking-[0.05em]">
-        {user.value ? (user.value.name ?? "Conta") : "Entrar"}
+        {user.value ? (user.value.givenName ?? "Conta") : "Entrar"}
       </span>
     </a>
   );
--- sdk/analytics.tsx
+++ sdk/analytics.tsx
@@ -1,6 +1,19 @@
-import type { AnalyticsEvent } from "apps/commerce/types.ts";
+import type { AnalyticsEvent, IEvent } from "apps/commerce/types.ts";
 
-export const sendEvent = <E extends AnalyticsEvent>(event: E) => {
+export interface UserParams {
+  email?: string;
+  name?: string;
+}
+
+export interface PostUserEvent extends IEvent<UserParams> {
+  name: "user_info";
+}
+
+type AnalyticsCustomEvents = PostUserEvent;
+
+export const sendEvent = <E extends AnalyticsEvent | AnalyticsCustomEvents>(
+  event: E,
+) => {
   console.log(JSON.stringify(event, null, 2));
   window.DECO.events.dispatch(event);
 };
