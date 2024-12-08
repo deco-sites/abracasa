da2668db7b4405bcbf7f349191f57014a7f56e2c
Carlos Garcia <carlos@go-allfa.com>
2024-12-08 14:25:37 +0000

    Publish 1 from staging into origin/main

--- components/ui/SectionNewsLetter.tsx
+++ components/ui/SectionNewsLetter.tsx
@@ -7,7 +7,7 @@ export interface Props {
 
 export default function SectionNewsLetter({ title, subtitle }: Props) {
   return (
-    <div class="bg-[#B9154C] text-white max-w-full flex items-center justify-center">
+    <div id="newsletter" class="bg-[#B9154C] text-white max-w-full flex items-center justify-center">
       <div class="w-full h-full bg-[#B9154C] items-center justify-center flex flex-col text-center my-1 py-1 px-6">
         <NewsletterForm title={title} subtitle={subtitle} />
       </div>
