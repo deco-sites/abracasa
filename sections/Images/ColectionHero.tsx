cc2128a48463f3d071d88f9ce2ed39e90083cb45
Ricardo Henrique Gonzaga <ricardo.gonzaga@agenciam3.com>
2025-03-10 13:05:39 -0300

    feat: add cta button and productshelf to colecao page

--- sections/Images/ColectionHero.tsx
+++ sections/Images/ColectionHero.tsx
@@ -4,6 +4,12 @@ import type { ImageWidget as LiveImage, RichText } from "apps/admin/widgets.ts";
 interface Props {
     title?: RichText;
     titleMobile?: RichText;
+    button?: {
+        link?: string;
+        title?: string;
+        target?: "_blank" | "_self";
+        isBold?: boolean;
+    };
     image?: {
         /** @description desktop otimized image */
         desktop?: LiveImage;
@@ -18,13 +24,23 @@ interface Props {
     }[];
 }
 
-function ColectionHero({ title, titleMobile, image }: Props) {
+function ColectionHero({ title, titleMobile, button, image }: Props) {
     return (
         <div class="mt-[72px] lg:mt-[172px] relative">
             <div class="max-w-[1210px] mx-auto">
                 <div class="flex flex-col gap-7 lg:mx-7 xl:mx-0">
                     <h2 class="font-semibold mx-6 lg:mx-0 hidden lg:block" dangerouslySetInnerHTML={{ __html: title ?? '' }} />
                     <h2 class="font-semibold mx-6 lg:mx-0 lg:hidden" dangerouslySetInnerHTML={{ __html: titleMobile ?? '' }} />
+                    {button && (
+                        <a
+                            href={button.link || "#"}
+                            target={button.target || "_blank"}
+                            class={`w-fit flex mx-6 lg:mx-0 items-center justify-center border border-black text-black hover:bg-black hover:text-white transition-colors duration-300 py-2.5 px-3 ${button.isBold ? "font-bold" : "font-medium"
+                                }`}
+                        >
+                            {button.title}
+                        </a>
+                    )}
                     <ul class="flex flex-col lg:flex-row items-center gap-7">
                         {image?.map((img, index) => (
                             <li class={`${index === 1 ? 'xl:absolute xl:right-0 xl:top-0' : ''}`}>
