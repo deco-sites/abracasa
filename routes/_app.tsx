d6927a1b2e124b719dd83ac0a013b7c9985f0d5e
Ricardo Henrique Gonzaga <ricardo.gonzaga@agenciam3.com>
2025-03-19 10:26:26 -0300

    fix: test changing gtm location

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,7 +1,7 @@
 import { AppProps } from "$fresh/server.ts";
 import GlobalTags from "$store/components/GlobalTags.tsx";
 import Theme from "$store/sections/Theme/Theme.tsx";
-
+import { Head } from "$fresh/runtime.ts";
 const sw = () =>
   addEventListener("load", () =>
     navigator && navigator.serviceWorker &&
@@ -10,6 +10,16 @@ const sw = () =>
 function App(props: AppProps) {
   return (
     <>
+      <Head>
+        <script type="text/javascript" dangerouslySetInnerHTML={{
+          __html: `
+      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s);j.async=true;j.src=i;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer', 'https://www.googletagmanager.com/gtm.js?id=GTM-KS9HHK');
+        `
+        }} />
+      </Head>
       {/* Include default fonts and css vars */}
       <Theme />
 
