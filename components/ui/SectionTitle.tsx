77a4420db45f947ffb6922c406215dd5f835242e
Naian Felix <naianfelix123@gmail.com>
2025-09-05 16:29:31 -0300

    feat: change style for button

--- components/ui/SectionTitle.tsx
+++ components/ui/SectionTitle.tsx
@@ -11,33 +11,33 @@ export interface Props {
 
 export default function SectionTitle({ title, titleMob, button }: Props) {
   return (
-    <div class="max-w-[1210px] h-full flex justify-start mx-auto flex-col mb-[72px] px-6 lg:px-0">  
-      <div class="font-inter font-bold mt-[70px] lg:mt-[150px]">
+    <div class="max-w-[1210px] h-full flex justify-center mx-auto flex-col lg:mb-[105px] mb-[72px] px-6 lg:px-0 mt-[74px] lg:mt-[104px]">
+      <div class="font-inter font-bold">
         <div
           class="hidden lg:block"
           dangerouslySetInnerHTML={{ __html: title }}
         />
         <div class="lg:hidden" dangerouslySetInnerHTML={{ __html: titleMob }} />
       </div>
-      <div class="flex lg:hidden justify-start lg:px-0 mt-[77px]">
+      <div class="flex lg:hidden justify-center lg:px-0 mt-[25px]">
         <a
           target="_self"
           rel="noopener noreferrer"
           href={button?.buttonLink ?? "#"}
           class={
-            "flex justify-center items-center btn bg-transparent uppercase rounded-none py-2 border-[1px] border-solid border-[#555555] text-center text-xs font-semibold w-full text-[#555555] max-w-[371px]"
+            "text-[13px] flex justify-center items-center cursor-pointer bg-black uppercase rounded-none py-2 border-[1px] border-solid border-[#555555] text-center text-xs font-semibold w-full text-white max-w-[406px]"
           }
         >
           <span>{button?.buttonText}</span>
         </a>
       </div>
-      <div class="hidden lg:flex justify-start lg:px-0">
+      <div class="hidden lg:flex justify-center lg:px-0 mt-[54px]">
         <a
           target="_self"
           rel="noopener noreferrer"
           href={button?.buttonLink ?? "#"}
           class={
-            "hidden lg:flex justify-center items-center mt-[77px] btn bg-transparent uppercase rounded-none py-2 border-[1px] border-solid border-[#555555] text-center text-[17px] font-semibold w-full text-[#555555] max-w-[371px]"
+            "hidden lg:flex text-[15px] justify-center items-center cursor-pointer bg-black uppercase rounded-none py-2 border-[1px] border-solid border-[#555555] text-center font-semibold w-full text-white max-w-[406px]"
           }
         >
           <span>{button?.buttonText}</span>
